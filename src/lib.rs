93a637122e38b2f5c830752f3084d9420b42133c
Michael Whittaker <mjw297@cornell.edu>
2014-12-16 20:18:48 +0000

    Initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,65 @@
+#![feature(macro_rules)]
+
+use std::fmt;
+use List::Nil;
+use List::Cons;
+
+enum List<A> {
+    Nil,
+    Cons(A, Box<List<A>>)
+}
+
+macro_rules! list[
+    ()                       => (Nil);
+    ($x:expr)                => (Cons($x, box Nil));
+    ($x:expr, $($xs:expr),+) => (Cons($x, box list!($($xs),+)));
+]
+
+impl<A> List<A> {
+    fn map<B>(&self, f: |&A| -> B) -> List<B> {
+        match *self {
+            Nil                 => Nil,
+            Cons(ref x, ref xs) => Cons (f(x), box xs.map(f))
+        }
+    }
+
+    fn fold_left<B>(&self, f: |B, &A| -> B, a: B) -> B {
+        match *self {
+            Nil                 => a,
+            Cons(ref x, ref xs) => {
+                let a = f(a, x);
+                xs.fold_left(f, a)
+            }
+        }
+    }
+
+    fn length(&self) -> int {
+        self.fold_left(|a, _| a + 1, 0)
+    }
+
+}
+
+impl<A: Clone> List<A> {
+    fn rev(&self) -> List<A> {
+        self.fold_left(|a, x| Cons(x.clone(), box a), Nil)
+    }
+}
+
+impl<A: fmt::Show> List<A> {
+    fn to_string(&self) -> String {
+        fn help<A: fmt::Show>(l: &List<A>) -> String {
+            match *l {
+                Nil                      => String::from_str(""),
+                Cons (ref x, box Nil)    => format!("{}", *x),
+                Cons (ref x, box ref xs) => format!("{}, {}", *x, help(xs))
+            }
+        }
+        format!("[{}]", help(self))
+    }
+}
+
+impl<A: fmt::Show> fmt::Show for List<A> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
+}
