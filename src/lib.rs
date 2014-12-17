bacacda9be41f8d095808e68de0d9b5ffd0a05ce
Michael Whittaker <mjw297@cornell.edu>
2014-12-17 05:48:24 +0000

    Renamed hd/tl. Added more test cases.

--- src/lib.rs
+++ src/lib.rs
@@ -25,23 +25,23 @@ impl<A> List<A> {
 }
 
 impl<A: Clone> List<A> {
-    pub fn head(&self) -> Option<A> {
+    pub fn hd(&self) -> Option<A> {
         match *self {
             Nil            => None,
             Cons(ref x, _) => Some(x.clone())
         }
     }
 
-    pub fn tail(&self) -> Option<List<A>> {
+    pub fn tl(&self) -> Option<List<A>> {
         match *self {
             Nil                 => None,
             Cons(_, box ref xs) => Some(xs.clone())
         }
     }
 
-    pub fn nth(&self, i: int) -> Option<A> {
-        panic!("TODO")
-    }
+    //pub fn nth(&self, i: int) -> Option<A> {
+    //    panic!("TODO")
+    //}
 
     pub fn rev(&self) -> List<A> {
         self.fold_left(|a, x| Cons(x.clone(), box a), Nil)
@@ -94,9 +94,55 @@ mod tests {
     #[test]
     fn macro_test() {
         let nil: List<int> = list![];
-        assert!(nil == Nil);
+        assert_eq!(nil,             Nil);
+        assert_eq!(list![1i],       Cons(1i, box Nil));
+        assert_eq!(list![1i, 2],    Cons(1i, box Cons(2, box Nil)));
+        assert_eq!(list![1i, 2, 3], Cons(1i, box Cons(2, box Cons(3, box Nil))));
 
-        assert!(list![1i] == Cons(1i, box Nil));
-        assert!(list![1i, 2] == Cons(1i, box Cons(2, box Nil)));
+        let nil: List<Box<int>> = list![];
+        assert_eq!(nil,                         Nil);
+        assert_eq!(list![box 1i],               Cons(box 1i, box Nil));
+        assert_eq!(list![box 1i, box 2],        Cons(box 1i, box Cons(box 2, box Nil)));
+        assert_eq!(list![box 1i, box 2, box 3], Cons(box 1i, box Cons(box 2, box Cons(box 3, box Nil))));
+    }
+
+    #[test]
+    fn lenght_test() {
+        let nil: List<int> = list![];
+        assert_eq!(nil                  .length(), 0);
+        assert_eq!(list![1i]            .length(), 1);
+        assert_eq!(list![1i, 2]         .length(), 2);
+        assert_eq!(list![1i, 2, 3]      .length(), 3);
+        assert_eq!(list![1i, 2, 3, 4]   .length(), 4);
+        assert_eq!(list![1i, 2, 3, 4, 5].length(), 5);
+    }
+
+    #[test]
+    fn hd_test() {
+        let nil: List<int> = list![];
+        assert_eq!(nil.hd(), None);
+        assert_eq!(list![1i]      .hd(), Some(1i));
+        assert_eq!(list![1i, 2]   .hd(), Some(1i));
+        assert_eq!(list![1i, 2, 3].hd(), Some(1i));
+    }
+
+    #[test]
+    fn tl_test() {
+        let nil: List<int> = list![];
+        assert_eq!(nil.tl(), None);
+        assert_eq!(list![1i]      .tl(), Some(list![]));
+        assert_eq!(list![1i, 2]   .tl(), Some(list![2i]));
+        assert_eq!(list![1i, 2, 3].tl(), Some(list![2i, 3]));
+    }
+
+    #[test]
+    fn rev_test() {
+        let nil: List<int> = list![];
+        assert_eq!(nil                  .rev(), nil);
+        assert_eq!(list![1i]            .rev(), list![1i]);
+        assert_eq!(list![1i, 2]         .rev(), list![2i, 1]);
+        assert_eq!(list![1i, 2, 3]      .rev(), list![3i, 2, 1]);
+        assert_eq!(list![1i, 2, 3, 4]   .rev(), list![4i, 3, 2, 1]);
+        assert_eq!(list![1i, 2, 3, 4, 5].rev(), list![5i, 4, 3, 2, 1]);
     }
 }
